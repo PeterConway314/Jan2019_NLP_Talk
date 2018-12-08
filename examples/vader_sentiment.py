c603ee36db81de5c34fe58d7bde3ee707b01040a
Peter <peterconway3.14@gmail.com>
2018-12-08 18:48:44 +0000

    Initial commit

---
+++ examples/vader_sentiment.py
@@ -0,0 +1,36 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+import pickle
+
+
+def get_lexicon():
+    with open('../lexicon.pickle', 'rb') as rf:
+        lexicon = pickle.load(rf)
+    return lexicon
+
+def calculate_sentiment(review):
+    lexicon = get_lexicon()
+
+    factors = []
+    for sentence in sent_tokenize(review):
+        sentFactors = []
+        for word in word_tokenize(sentence):
+            if word in lexicon:
+                sentFactors.append(lexicon[word])
+        print(sentFactors)
+
+"""
+-would be good to store the word and score somewhere to view
+-need to determine how to calculate the average of all 'factors'
+-rename 'factors' to something clearer
+-provide a bank of example movie reviews
+"""
+
+
+
+
+
+if __name__ == "__main__":
+    review = "Always Sunny is off to a strong start now that it's proven that it can still pull off twists after more than a decade on the air."
+    review2 = "what a great game, it's inspiring how much they crammed into this gem! I can't wait to enjoy the sequel."
+
+    calculate_sentiment(review2)
