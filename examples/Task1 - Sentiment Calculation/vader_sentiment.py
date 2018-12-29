652ef872ff791676a12936cce3d5493b7f9062a3
Peter <peterconway3.14@gmail.com>
2018-12-29 15:49:50 +0000

    Implemented 2 methods to calculate sentiment.

--- examples/Task1 - Sentiment Calculation/vader_sentiment.py
+++ examples/Task1 - Sentiment Calculation/vader_sentiment.py
@@ -3,34 +3,49 @@
 from nltk.tokenize import word_tokenize, sent_tokenize
 from vader_lexicon import loadLexicon
 
-def calculate_sentiment(review):
+def get_features(review):
     lexicon = loadLexicon()
 
-    input(lexicon)
-
-    factors = []
+    features = []
     for sentence in sent_tokenize(review):
-        sentFactors = []
+        sentFeatures = []
         for word in word_tokenize(sentence):
             if word in lexicon:
-                sentFactors.append(lexicon[word])
-        #Do some maths to calculate score.
-        print(sentFactors)
+                sentFeatures.append((word,lexicon[word]))
+        #Do some maths to calculate score. See functions below.
+        # print(sentFeatures)
+        features.append(sentFeatures)
 
-"""
--would be good to store the word and score somewhere to view
--need to determine how to calculate the average of all 'factors'
--rename 'factors' to something clearer
-
--provide a bank of steam reviews
-"""
+    return features
 
 
+def feat_to_score_1(features):
+    """
+    Average the sentiment for sentences, then average for all sentences.
+    """
+    scores = []
+    for sent in features:
+        sentScores = [item[1] for item in sent]
+        avgSentScore = sum(sentScores) / len(sentScores)
+        scores.append(avgSentScore)
+    sentiment = sum(scores) / len(scores)
+    return sentiment
 
+def feat_to_score_2(features):
+    """
+    Simple average of all features in text.
+    """
+    scores = []
+    for sent in features:
+        scores.extend([item[1] for item in sent])
+    sentiment = sum(scores) / len(scores)
+    return sentiment
 
 
 if __name__ == "__main__":
     review = "Always Sunny is off to a strong start now that it's proven that it can still pull off twists after more than a decade on the air."
     review2 = "what a great game, it's inspiring how much they crammed into this gem! I can't wait to enjoy the sequel."
 
-    calculate_sentiment(review2)
+    features = get_features(review2)
+    print("Method 1: {}".format(feat_to_score_1(features)))
+    print("Method 2: {}".format(feat_to_score_2(features)))
