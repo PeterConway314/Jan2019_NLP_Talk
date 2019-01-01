a3a8330768be20dc196ae53ebab7d5f00bf40b22
Peter <peterconway3.14@gmail.com>
2019-01-01 12:19:26 +0000

    Finished re-write. ~70% accurate.

--- examples/Task2 - Sentiment Classification/nb_classifier.py
+++ examples/Task2 - Sentiment Classification/nb_classifier.py
@@ -1,51 +1,7 @@
-# import nltk
-# from nltk.corpus import movie_reviews
-# import random
-# from nltk.tokenize import word_tokenize
-
-# documents = [
-#     (list(movie_reviews.words(fileid)), category)
-#     for category in movie_reviews.categories()
-#     for fileid in movie_reviews.fileids(category)
-# ]
-# random.shuffle(documents)
-
-# # word_features = movie_reviews.words()
-# words = nltk.FreqDist(word.lower() for word in movie_reviews.words())
-# word_features = list(words)[:2000]
-
-# def document_features(document):
-#     document_words = set(document)
-#     features = {}
-#     for word in word_features:
-#         # features['contains({})'.format(word)] = (word in document_words)
-#         features[word] = (word in document_words)
-#     # input(features)
-#     return features
-
-
-# featuresets = [(document_features(d), c) for (d,c) in documents]
-# train_set, test_set = featuresets[100:], featuresets[:100]
-
-# classifier = nltk.NaiveBayesClassifier.train(train_set)
-
-# # print(nltk.classify.accuracy(classifier, test_set))
-# classifier.show_most_informative_features(5)
-
-
-# print(classifier.classify(document_features("""A thoroughly entertaining ride.""")))
-
-# # print(classifier.classify(document_features(word_tokenize("what an amazing movie, brilliant from start to finish!"))))
-
-
-
-
-
 import os
 dataPath = os.path.join(__file__,'..','..','..','data','steam_reviews')
 posData = os.path.join(dataPath,'pos.txt')
 negData = os.path.join(dataPath,'neg.txt')
-from collections import Counter
 import nltk
 from nltk.tokenize import word_tokenize
 import random
@@ -58,119 +14,50 @@ stopWords = list(set(stopwords.words('english')))
 stopWords.extend(string.punctuation)
 
 
-def clean_reviews(reviews):
+#Collect documents:
+documents = []
+#Load positive review data:
+with open(posData, 'r', encoding='utf-8') as rf:
+    posDocs = [(word_tokenize(review), 'pos') for review in rf.readlines()]
+    random.shuffle(posDocs)
+documents.extend(posDocs)
+#Load negative review data:
+with open(negData, 'r', encoding='utf-8') as rf:
+    negDocs = [(word_tokenize(review), 'neg') for review in rf.readlines()]
+    random.shuffle(negDocs)
+documents.extend(negDocs)
+
+
+def word_feats(words):
     """
+    A function to convert a word tokenized list to the 'word feature'
+    format expected by the classifier.
     """
-    reviews = [review.lower() for review in reviews] #Lower-case text.
-    reviews = [review.strip() for review in reviews] #Remove '\n', etc.
-    return reviews
-
-def generate_vocabulary(posPath, negPath, featureSize=5000):
-    """
-    """
-    reviews = [] #To contain ALL reviews.
-    #Load positive review data:
-    with open(posPath, 'r', encoding='utf-8') as rf:
-        reviews.extend(rf.readlines())
-    #Load negative review data:
-    with open(negPath, 'r', encoding='utf-8') as rf:
-        reviews.extend(rf.readlines())
-
-    #Clean reviews - ensures 'smarter' vocabulary:
-    reviews = clean_reviews(reviews)
-
-    #Instantiate a word counter and count occurrences of words:
-    wordCounter = Counter()
-    for review in reviews:
-        wordCounter.update(word_tokenize(review))
-
-    #Remove stopwords from our counter:
-    for word in stopWords:
-        if word in wordCounter:
-            del wordCounter[word]
-
-    print("No. Unique Words={}".format(len(wordCounter)))
-    print("Most common words:\n{}".format(wordCounter.most_common(10)))
-    wordFeatures = wordCounter.most_common(featureSize)
-
-    #Remove counts, to return only words:
-    words = [w[0] for w in wordFeatures]
-
-    return words
-
-def format_documents(posPath, negPath):
-    """
-    """
-    documents = [] #To contain document data.
-    #Load positive review data:
-    with open(posPath, 'r', encoding='utf-8') as rf:
-        reviews = [(word_tokenize(review), 'pos') for review in rf.readlines()]
-        documents.extend(reviews)
-    #Load negative review data:
-    with open(negPath, 'r', encoding='utf-8') as rf:
-        reviews = [(word_tokenize(review), 'neg') for review in rf.readlines()]
-        documents.extend(reviews)
-
-    random.shuffle(documents)
-    return documents
-
-def document_features(document, vocabulary):
-    """
-    """
-    document_words = set(document)
-    features = {}
-
-    for word in vocabulary:
-        features[word] = (word in document_words)
-    # input(features)
-    return features
-
-def train_classifier(evalRatio=0.1):
-    """
-    """
-    #Generate vocabulary:
-    vocab = generate_vocabulary(posData, negData)
-
-    #Re-format documents:
-    all_documents = format_documents(posData, negData)
-    all_documents = [(document_features(d, vocab), c) for (d,c) in all_documents]
-
-    #Create training & evaluation sets:
-    split_index = int(evalRatio * len(all_documents))
-    train_documents = all_documents[split_index:]
-    eval_documents = all_documents[:split_index]
-    print("\nTotal docs={}\nTrain docs={}, Eval docs={}".format(
-        len(all_documents),
-        len(train_documents), len(eval_documents)
-    ))
-
-    #Train our `classifier`:
-    print("Training classifier...")
-    classifier = nltk.NaiveBayesClassifier.train(train_documents)
-
-    #Display some most informative features:
-    classifier.show_most_informative_features(50)
-
-    print("acc={}".format(nltk.classify.accuracy(classifier, eval_documents)))
-
-    review1 = "great game, lots of good visuals my friends love it"
-    review2 = ""
-    r1_feats = document_features(word_tokenize(review1), vocab)
-    r2_feats = document_features(word_tokenize(review2), vocab)
-
-    print(classifier.classify(r1_feats))
+    return dict([(word.lower(), True) for word in words if word not in stopWords])
 
 
-    #.pickle classifier to avoid re-training:
+#Construct our datasets for training and evaluation:
+train_data = posDocs[50:1050] + negDocs[50:1050]
+eval_data = posDocs[:50] + negDocs[:50]
+train_set = [(word_feats(rev), category) for (rev, category) in train_data]
+eval_set = [(word_feats(rev), category) for (rev, category) in eval_data]
 
+#Train our classifier:
+classifier = nltk.NaiveBayesClassifier.train(train_set)
 
-def restore_classifier(classifier='nb_classifier.pickle'):
-    """
-    """
-    pass
+#Perform some basic analysis:
+print("Naive Bayes Algo accuracy: {}".format(
+    nltk.classify.accuracy(classifier, eval_set)
+))
+classifier.show_most_informative_features(15)
 
+#Test our model before deciding to .pickle it:
+test_pos_review = "great game! i had a lot of fun with the multiplayer."
+test_neg_review = "waste of money. you shouldn't buy this game in its current state."
+feats = word_feats(word_tokenize(test_neg_review))
+classification = classifier.classify(feats)
+print(classification)
 
-if __name__ == "__main__":
-    train_classifier()
-    # print(format_documents(posData, negData))
-    # generate_vocabulary(posData, negData)
+#.pickle (save) our model for the future:
+with open('classifier.pickle', 'wb') as wf:
+    pickle.dump(classifier, wf)
